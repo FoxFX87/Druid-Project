9ae55ae55b1321d1c934cdca87f7238706f5c554
FoxFX87 <nmahukanna@gmail.com>
2021-06-28 12:50:57 -0400

    Testing Projectile Effects code and after effects

--- Druid- Project/src/Projectiles/BaseProjectile.gd
+++ Druid- Project/src/Projectiles/BaseProjectile.gd
@@ -4,14 +4,22 @@ class_name Projectile
 export (int) var speed = 200
 var direction : Vector2 setget _set_direction
 
+onready var part = $ProjectileParticle/AnimationPlayer
+
 func _process(delta):
 	position += speed * direction * delta
 	
 func _set_direction(dir : Vector2):
 	direction = dir
 
-func _on_BaseProjectile_area_entered(_area):
+func _destroy():
+	speed = 0
+	part.play("Fade")
+	yield(part,"animation_finished")
 	queue_free()
 
+func _on_BaseProjectile_area_entered(_area):
+	 queue_free()
+
 func _on_BaseProjectile_body_entered(_body):
-	queue_free()
+	 queue_free()
