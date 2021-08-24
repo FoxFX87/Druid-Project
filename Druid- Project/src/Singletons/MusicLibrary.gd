8bddac146ccca8734471345a0ce58555a0649dd1
FoxFX87 <nmahukanna@gmail.com>
2021-08-24 00:13:25 -0400

    Redesign Stage001

--- Druid- Project/src/Singletons/MusicLibrary.gd
+++ Druid- Project/src/Singletons/MusicLibrary.gd
@@ -1,5 +1,12 @@
 extends Node
 
+var current_track = null
+
 func _play(track = null):
 	if track:
-		get_node(track).play()
+		
+		if current_track != track:
+			current_track = track
+			get_node(track).play()
+		else:
+			return
