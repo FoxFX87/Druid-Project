ddd6de6fd35558e2d1514bd5e5cefc7ea4d8c098
FoxFX87 <nmahukanna@gmail.com>
2021-09-19 23:21:32 -0400

    Tilemap second wrap up

--- Druid- Project/Source 9-3-2021/Player/Player.gd
+++ Druid- Project/Source 9-3-2021/Player/Player.gd
@@ -53,13 +53,20 @@ func _process(_delta):
 				_transition_to_state(STATE.PREPARE_ATTACK)
 				
 		STATE.ATTACK:
-			pass
+			anim.play("Attack")
 			
 		STATE.PREPARE_ATTACK:
 			anim.play("Prepare Attack")
 			
 			if Input.is_action_just_pressed("in_attack"):
 				_transition_to_state(STATE.MOVE)
+				
+			for dir in inputs.keys():
+				if Input.is_action_just_pressed(dir):
+					_update_move_ray(dir)
+					move_vector = inputs[dir]
+					_update_sprite_direction()
+					_transition_to_state(STATE.ATTACK)
 				
 		STATE.PUSH:
 			anim.play("Push")
@@ -96,3 +103,26 @@ func _update_move_ray(dir):
 	var vector_pos = inputs[dir] * CELL_SIZE
 	move_ray.cast_to = vector_pos
 	move_ray.force_raycast_update()
+
+func _get_special_floors():
+	
+	var detected_floor = "DEFAULT"
+	
+	for b in get_overlapping_bodies():
+		if b is TileMap:
+			var tile_pos = b.world_to_map(position)
+			var tile_id = b.get_cellv(tile_pos)
+			var tile_name = b.tile_set.tile_get_name(tile_id)
+			
+			match tile_name:
+				"Grass":
+					detected_floor = "GRASS"
+				"Gravel":
+					detected_floor = "GRAVEL"
+				"Water":
+					detected_floor = "WATER"
+	print(detected_floor)
+
+func _on_TweenMove_tween_all_completed():
+	_get_special_floors()
+
